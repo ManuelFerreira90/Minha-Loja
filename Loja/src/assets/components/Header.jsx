1f1eadc6defa1397be55689e9c516774545ad5ac
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2023-08-05 10:20:03 -0300

    header

---
+++ Loja/src/assets/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {BsSearch, BsCart2} from 'react-icons/bs'
+import '../css/Header.css'
+
+function Header() {
+  return (
+    <header>
+
+        <div className='container'>
+
+            <form className='search_container'>
+                <input type="search" placeholder='Buscar produtos' className='input_produtos' required/>
+                <button className='search_btn'> 
+                    <BsSearch/> 
+                </button>
+            </form>
+
+            <button className='cart_btn'> 
+                <BsCart2/>
+                <span className='cart_info'>1</span> 
+            </button>
+            
+        </div>
+
+    </header>
+  )
+}
+
+export default Header

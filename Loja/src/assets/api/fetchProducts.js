0cc55b149bb4161b5d76e9b65f8a271fcc821028
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2023-08-09 19:33:53 -0300

    finalized

--- Loja/src/assets/api/fetchProducts.js
+++
@@ -1,8 +0,0 @@
-const fetchProdutos = async (query) => {
-    const response = await fetch(`https://fakestoreapi.com/products/${query}`)
-    const data = await response.json()
-
-    return data
-}
-
-export default fetchProdutos

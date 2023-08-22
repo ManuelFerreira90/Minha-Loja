f123d7cf3e40bb2f0f9d94808ed0d021c0c1cc7d
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2023-08-22 00:02:19 -0300

    update

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 import useAppContext from '../hook/useAppContext'
 import { Link, useLocation  } from 'react-router-dom'
 import '../css/NavBar.css'
@@ -14,9 +14,19 @@ function NavBar() {
 
   const location = useLocation();
   const pathSegments = location.pathname.split('/');
+  const lastSegment = pathSegments[pathSegments.length - 1];
+  const isHomePage = lastSegment === 'Minha-Loja';
 
-  const baseSegment = pathSegments[1];
-  const isHomePage = baseSegment === 'Minha-Loja';
+  const [homePage, setHomePage] = useState(false);
+
+  useEffect(() => {
+    if (isHomePage) {
+      setHomePage(false)
+    }
+    else{
+      setHomePage(true)
+    }
+  }, [isHomePage]);
 
   const [active, setActive] = useState('1')
 
@@ -49,7 +59,7 @@ function NavBar() {
     <div>
         <nav className='products_container'>
             {
-              search || isHomePage ?
+              search || homePage ?
                 <ul className='products_list'>
                   <li 
                     className='products_section'
